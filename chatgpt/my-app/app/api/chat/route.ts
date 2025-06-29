07fe394dcd576dbfb9a82e7b20850b12589b8e95
Shivam Rathour <133578439+ShivamgithubRathour@users.noreply.github.com>
2025-06-29 22:03:43 +0530

    Done

---
+++ chatgpt/my-app/app/api/chat/route.ts
@@ -0,0 +1,39 @@
+// import { openai } from "@ai-sdk/openai";
+// import { frontendTools } from "@assistant-ui/react-ai-sdk";
+// import { streamText } from "ai";
+
+// export const runtime = "edge";
+// export const maxDuration = 30;
+
+// export async function POST(req: Request) {
+//   const { messages, system, tools } = await req.json();
+
+//   const result = streamText({
+//     model: openai("gpt-4o"),
+//     messages,
+//     // forward system prompt and tools from the frontend
+//     toolCallStreaming: true,
+//     system,
+//     tools: {
+//       ...frontendTools(tools),
+//     },
+//     onError: console.log,
+//   });
+
+//   return result.toDataStreamResponse();
+// }
+
+
+import { google } from "@ai-sdk/google";
+import { streamText } from "ai";
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  const result = streamText({
+    model: google("gemini-2.0-flash"),
+    messages,
+  });
+  return result.toDataStreamResponse();
+}
